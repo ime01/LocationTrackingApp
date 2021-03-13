f73c6a4234b361017a85b77d9346ed25cc87589b
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-03-12 17:01:02 -0800

    Added MapView to constantly display current location using LiveData

--- app/src/main/java/com/flowz/locationtrackingapp/MainActivity.kt
+++ app/src/main/java/com/flowz/locationtrackingapp/MainActivity.kt
@@ -5,27 +5,39 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.flowz.locationtrackingapp.ui.LocationViewModel
 import com.flowz.locationtrackingapp.util.GpsUtils
 import com.flowz.locationtrackingapp.util.GpsUtils.Companion.GPS_REQUEST
 import com.flowz.locationtrackingapp.util.GpsUtils.Companion.LOCATION_REQUEST
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var locationViewModel: LocationViewModel
+    private lateinit var mapView: MapView
     private var isGPSEnabled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initGoogleMap(savedInstanceState)
+
 
         locationViewModel = ViewModelProviders.of(this).get(LocationViewModel::class.java)
 
@@ -36,12 +48,24 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        invokeLocationAction()
+    private fun initGoogleMap(savedInstanceState: Bundle?){
+
+        mapView = map_view
+
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY)
+        }
+
+        mapView.onCreate(mapViewBundle)
+
+        mapView.getMapAsync(this)
+
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -55,11 +79,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun invokeLocationAction() {
         when{
-            !isGPSEnabled -> lat_long.text = "Please Eanble your Gps"
+            !isGPSEnabled -> lat_long.text = getString(R.string.enable_gps)
 
             isPermissionsGranted() -> startLocationUpdate()
 
-            shouldShowRequestPermissionRationale() -> lat_long.text = "Please grant App Gps Permisson"
+            shouldShowRequestPermissionRationale() -> lat_long.text = getString(R.string.grant_permission)
 
             else-> ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_REQUEST)
         }
@@ -69,7 +93,7 @@ class MainActivity : AppCompatActivity() {
     private fun startLocationUpdate(){
         locationViewModel.fetchLocationData().observe(this, Observer {
 
-            lat_long.text = (it.longitude ).toString()+ (it.latitude).toString()
+            lat_long.text = getString(R.string.current_location) + (it.longitude ).toString()+ (it.latitude).toString()
         })
     }
 
@@ -101,6 +125,92 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    companion object{
+        const val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+        mapView.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+//        googleMap.addMarker(MarkerOptions().position(StartLocation).title("StartLocation"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(StartLocation))
+//        googleMap.addPolyline(PolylineOptions()
+//                .clickable(true)
+//                .add(StartLocation)
+//                .add(StopLocation)
+//                .width(8f)
+//                .color(resources.getColor(R.color.colorAccent))
+//        )
+//        googleMap.addCircle(CircleOptions()
+//                .center(StopLocation)
+//                .radius(50000.0)
+//                .strokeWidth(3f)
+//                .strokeColor(resources.getColor(R.color.colorAccent))
+//                .fillColor(resources.getColor(R.color.colorAccent)))
+
+        locationViewModel.fetchLocationData().observe(this, Observer {
+
+            googleMap.addMarker(MarkerOptions().position(LatLng(it.latitude, it.longitude)))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(it.latitude, it.longitude)))
+
+        })
+
+
+//
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        googleMap.isMyLocationEnabled = true
+        Toast.makeText(this, getString(R.string.current), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+        invokeLocationAction()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onPointerCaptureChanged(hasCapture: Boolean) {}
 
 
 }
